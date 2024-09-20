10d58d739949e8c172056c4984ed096b5842dd09
devCaiodias <caiodiasmartins12@gmail.com>
2024-09-20 18:19:59 -0300

    criar um banco de dados que n é banco de dados para o selectForme

--- src/components/form/SelectForm.js
+++ src/components/form/SelectForm.js
@@ -6,6 +6,9 @@ function SelectForm( {text, name, options, handleOnChange, value}) {
             <label htmlFor={name}>{text}:</label>
             <select name={name} id={name}>
                 <option>Seleciona uma opção </option>
+                {options.map((option) => (
+                    <option value={option.id} key={option.id}> {option.name}</option>
+                ))}
             </select>
             
         </div>
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,14 +1,33 @@
+import { useEffect, useState } from 'react';
+
 import Input from '../form/InputForm.js';
 import SelectForm from '../form/SelectForm.js';
 import SubmitForm from '../form/SubmitForm.js';
 import styles from './ProjectForm.module.css'
 
 function ProjectForm( {btnText} ) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((err) => console.log(err))
+    },[])
+
     return (
         <form className={styles.form}>
             <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto" />
             <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Insira o Orçamento total" />
-            <SelectForm name="category_id" text="Selecione a categoria" />
+            <SelectForm name="category_id" text="Selecione a categoria" options={categories} />
             <SubmitForm text={btnText} />
         </form>
     )

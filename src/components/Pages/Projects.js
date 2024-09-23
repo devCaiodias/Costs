95bd0a530a92bafc7f6b02b320138ff8903e5677
devCaiodias <caiodiasmartins12@gmail.com>
2024-09-23 17:57:58 -0300

    Dando uma introducao o da nossa pagina Projects. js

--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -1,6 +1,8 @@
 import { useLocation } from "react-router-dom";
-
+import styles from './Projects.module.css';
 import Mensagem from "../layout/Menssagem"; 
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
 
 function Projects() {
     const location = useLocation();
@@ -12,9 +14,15 @@ function Projects() {
     }
 
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/NewProjet" text="Criar Projeto" />
+            </div>
             {mensagem && <Mensagem msg={mensagem} type="sucesso" />}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
     );
 }
